76323b565efaa6a880bf0dd5b9080411b6044916
AWildTeddyBear <S7R1K3RStudios@gmail.com>
2018-09-05 19:24:07 -0500

    Update. Add colors and such to output.

--- Classwork Stuff/8-30-2018/main.cpp
+++ Classwork Stuff/8-30-2018/main.cpp
@@ -17,6 +17,26 @@
 #include <ctime>
 #include <chrono>
 
+#define RST "\x1B[0m" //Reset (No colour)
+#define KRED "\x1B[31m" //Red
+#define KGRN "\x1B[32m" //Green
+#define KYEL "\x1B[33m" //Yellow
+#define KBLU "\x1B[34m" //Blue
+#define KMAG "\x1B[35m" //Magenta
+#define KCYN "\x1B[36m" //Cyan
+#define KWHT "\x1B[37m" //White
+#define KUNDL "\x1B[4m" //Underlined
+#define KBOLD "\x1B[1m" //Bold
+#define FRED(x) KRED x RST //Red + Reset
+#define FGRN(x) KGRN x RST //Green + Reset
+#define FYEL(x) KYEL x RST //Yellow + Reset
+#define FBLU(x) KBLU x RST //Blue + Reset
+#define FMAG(x) KMAG x RST //Magenta + Reset
+#define FCYN(x) KCYN x RST //Cyan + Reset
+#define FWHT(x) KWHT x RST //White + Reset
+#define BOLD(x) "\x1B[1m" x RST //Bold + Reset
+#define UNDL(x) "\x1B[4m" x RST //Underlined + Reset
+
 // Function prototypes
 void mergeSort(int arr[], int leftIndex, int rightIndex), merge(int arr[], int leftIndex, int middle, int rightIndex);
 
@@ -29,9 +49,9 @@ int main(void){
     for (int i = 0; i < 1000; randomArr[i++] = (rand() % 1000) + 1);
 
     // Print our inital array to use it as a visual reference
-    std::cout << "Inital Array: " << std::endl;
+    std::cout << KBOLD << KGRN << "Inital Array: " << RST << KMAG << std::endl;
     for (int i = 0; i < arrSize; i++) std::cout << randomArr[i] << " "; 
-    std::cout << std::endl << std::endl;
+    std::cout << RST << std::endl << std::endl;
 
     // Call our mergesort function -- pass our array, and beginning and end index
     // Also measure the execution time of the function
@@ -41,9 +61,9 @@ int main(void){
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(currentAfter - currentBefore).count();
 
     // Print our our sorted array
-    std::cout << "Sorted Array: " << std::endl;
+    std::cout << KBOLD << KGRN << "Sorted Array: " << RST << KCYN << std::endl;
     for (int i = 0; i < arrSize; i++) std::cout << randomArr[i] << " ";
-    std::cout << std::endl << std::endl << "Total exeuction time of mergeSort with [" << arrSize + 1 << "] elements in the array: " << duration << " microseconds." << std::endl;
+    std::cout << RST << std::endl << std::endl << FMAG("Total exeuction time of mergeSort with ") << "[" << KGRN << arrSize + 1 << RST << "] " << FMAG("elements in the array: ") << KRED << KUNDL << duration << RST << KMAG << " microseconds." << RST << std::endl;
 
     return 0;
 }
